1572de7855047654c85f6f1ca402328d46f61b22
udulee <uduleenayanamalee@gmail.com>
2026-02-20 13:02:21 +0530

    fix bugs

---
+++ 11_Back_End/src/main/java/lk/ijse/backend/util/APIResponse.java
@@ -0,0 +1,14 @@
+package lk.ijse.backend.util;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class APIResponse<T> {
+    private int status;
+    private String message;
+    private T data;
+}
